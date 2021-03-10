5c8bcb3e8ec68f80261fc45e72e95f0f7dea307b
Kevin Rocker <kevin.j.rocker@gmail.com>
2021-03-09 23:08:44 -0500

    Update Keymap

--- keyboards/massdrop/alt/keymaps/kjrocker/keymap.c
+++ keyboards/massdrop/alt/keymaps/kjrocker/keymap.c
@@ -19,7 +19,6 @@ enum alt_keycodes {
     DBG_KBD,            //DEBUG Toggle Keyboard Prints                              //
     DBG_MOU,            //DEBUG Toggle Mouse Prints                                 //
     MD_BOOT,            //Restart into bootloader after hold timeout                //Working
-    QWERTY,             //Switch to QWERTY layout
     DVORAK,             //Switch to Dvorak layout
 };
 
@@ -34,19 +33,21 @@ enum alt_keycodes {
 #define TG_NUMP  TG(_NUMPAD)     // Toggle numpad layer
 #define OSL_LAY  OSL(_LAYOUT)    // One-shot layer to change layout
 
+#define QWERTY   DF(_QWERTY)
+
 // Layouts assume the OS is set to Dvorak.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DVORAK] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-        CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO_FUNC, KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [_QWERTY] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUOT, KC_RBRC, KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_X,    KC_COMM, KC_D,    KC_O,    KC_K,    KC_T,    KC_F,    KC_G,    KC_S,    KC_R,    KC_MINS, KC_EQL,  KC_BSLS, KC_HOME, \
-        CTL_ESC, KC_A,    KC_SCLN, KC_H,    KC_Y,    KC_U,    KC_J,    KC_C,    KC_V,    KC_P,    KC_Z,    KC_Q,             KC_ENT,  KC_PGUP, \
+        KC_ESC,  KC_A,    KC_SCLN, KC_H,    KC_Y,    KC_U,    KC_J,    KC_C,    KC_V,    KC_P,    KC_Z,    KC_Q,             KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_SLSH, KC_B,    KC_I,    KC_DOT,  KC_N,    KC_L,    KC_M,    KC_W,    KC_E,    KC_LBRC, KC_RSFT,          KC_UP,   KC_PGDN, \
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO_FUNC, KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
         _______, L_PSD,   L_BRI,   L_PSI,   L_T_BR,  _______, _______, _______, U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, TG_NUMP, KC_END, \
         KC_CAPS, L_PTP,   L_BRD,   L_PTN,   L_T_PTD, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
-        _______, L_T_MD,  L_T_ONF, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, OSL_LAY, _______,          KC_PGUP, KC_VOLD, \
+        _______, L_T_MD,  L_T_ONF, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, QWERTY , DVORAK , _______, _______,          KC_PGUP, KC_VOLD, \
         _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END  \
     ),
     [_NUMPAD] = LAYOUT(
@@ -90,11 +91,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
 
     switch (keycode) {
-        case QWERTY:
-            if (record->event.pressed) {
-                set_single_persistent_default_layer(_QWERTY);
-            }
-            return true;
         case DVORAK:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_DVORAK);
